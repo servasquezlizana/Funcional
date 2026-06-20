5302d537f72bf5d1e7d80c46f36a82a2a146a859
Sergio Vasquez <servasquezlizana@utn,frba.edu.com>
2026-06-20 10:58:54 -0300

    Sigo con el tp de pisteando

--- TP/tp-funcional-pisteando/src/Library.hs
+++ TP/tp-funcional-pisteando/src/Library.hs
@@ -1,22 +1,228 @@
 module Library where
 import PdePreludat
 
--- data autos
+{- ===================== ENTREGA 1 ===================== -}
+-- 1. Data autos
 data Auto = Auto {
     marca :: String,
     modelo :: String,
     desgasteRuedas :: Number,
     desgasteChasis :: Number,
     velocidadMaxima :: Number,
-    listaApodos :: [String],
-    tiempoEnPista :: Number
-}
+    apodos :: [String],
+    tiempoCarrera :: Number
+} deriving (Show)
+
+-- 2. Estado de salud del auto
+esPeugeot :: Auto -> Bool
+esPeugeot = (== "Peugeot") . marca
+
+pocoTiempoEnPista :: Auto -> Bool
+pocoTiempoEnPista = (< 100) . tiempoCarrera
 
 estaEnBuenEstado :: Auto -> Bool
 estaEnBuenEstado auto 
-    | ((== "Peugeot") . marca) auto     = False
-    | ((< 100) . tiempoEnPista) auto    = ((< 20) . desgasteChasis) auto
-    | otherwise                         = ((< 40) . desgasteChasis) auto && ((< 60) . desgasteRuedas) auto
+                | esPeugeot auto            = False
+                | pocoTiempoEnPista auto    = ((< 20) . desgasteChasis) auto
+                | otherwise                 = ((< 40) . desgasteChasis) auto && ((< 60) . desgasteRuedas) auto
+
+primerApodo :: [String] -> String
+primerApodo = head
+
+primeraPalabra :: [String] -> String
+primeraPalabra = head
 
 noDaMas :: Auto -> Bool
-noDaMas auto 
+noDaMas auto
+    | empiezaConLa (head (apodos auto)) = desgasteChasis auto > 80
+    | otherwise                         = desgasteRuedas auto > 80
+  where
+    empiezaConLa apodo = take 3 apodo == "La "
+
+esChiche :: Auto -> Bool
+esChiche auto
+    | even (length (apodos auto)) = desgasteChasis auto < 20
+    | otherwise                   = desgasteChasis auto < 50
+
+esJoya :: Auto -> Bool
+esJoya auto = ((== 0) . desgasteChasis) auto && ((== 0) . desgasteRuedas) auto && ((< 2).length.apodos) auto
+
+nivelDeChetez :: Auto -> Number
+nivelDeChetez auto = ((* 20).(length.apodos)) auto * ((length.modelo) auto)
+
+capacidadSupercalifragilisticaespialidosa :: Auto -> Number
+capacidadSupercalifragilisticaespialidosa auto = (length.primerApodo.apodos) auto
+
+riesgoso :: Auto -> Number
+riesgoso auto 
+                | estaEnBuenEstado auto = (velocidadMaxima auto) * (desgasteRuedas auto / 10)
+                | otherwise             = (velocidadMaxima auto) * (desgasteRuedas auto / 10) * 2
+
+-- 3. Manos a la obra
+
+reparar :: Auto -> Auto
+reparar auto = auto 
+    {
+        desgasteChasis = ((* 0.15).desgasteChasis) auto,
+        desgasteRuedas = 0
+    }
+
+penalizar :: Number -> Auto -> Auto
+penalizar penalidad auto = auto { tiempoCarrera = ((+ penalidad).tiempoCarrera) auto }
+
+ponerNitro :: Auto -> Auto
+ponerNitro auto = auto { velocidadMaxima = ((* 1.2).velocidadMaxima) auto }
+
+bautizar :: String -> Auto -> Auto
+bautizar apodonuevo auto = auto { apodos = apodos auto ++ [apodonuevo] }
+
+desarmadero :: String -> String -> Auto -> Auto
+desarmadero marcanueva modelonuevo auto = auto 
+    {
+        marca = marcanueva,
+        modelo = modelonuevo,
+        apodos = ["Nunca Taxi"]
+    }
+
+-- 4. Pistas
+type Tramo = Auto -> Auto
+data Pista = Pista 
+    {
+        nombre :: String,
+        pais :: String,
+        precioEntrada :: Number,
+        tramos :: [Tramo]
+    } deriving (Show)
+
+modificoTiempo :: (Number -> Number) -> Tramo
+modificoTiempo funcionModificadora auto = auto { tiempoCarrera = (funcionModificadora.tiempoCarrera) auto }
+
+modificoDesgasteRuedas :: (Number -> Number) -> Tramo
+modificoDesgasteRuedas funcionModificadora auto = auto { desgasteRuedas = (funcionModificadora.desgasteRuedas) auto}
+
+modificoDesgasteChasis :: (Number -> Number) -> Tramo
+modificoDesgasteChasis funcionModificadora auto = auto { desgasteChasis = (funcionModificadora.desgasteChasis) auto}
+
+curva :: Number -> Number -> Tramo
+curva angulo longitud auto = (modificoDesgasteRuedas (+ 3 * longitud / angulo) . modificoTiempo (+ longitud / ( velocidadMaxima auto / 2 ))) auto
+
+curvaPeligrosa :: Tramo
+curvaPeligrosa = curva 60 300
+
+curvaTranca :: Tramo
+curvaTranca = curva 110 550
+
+tramoRecto :: Number -> Tramo
+tramoRecto distancia auto = (modificoTiempo (+ (distancia / velocidadMaxima auto)) . modificoDesgasteChasis (+ (distancia / 100))) auto
+
+tramoRectoClassic :: Tramo
+tramoRectoClassic = tramoRecto 715
+
+tramito :: Tramo
+tramito = tramoRecto 260
+
+zigzag :: Number -> Tramo
+zigzag cambios auto = (modificoDesgasteChasis (+ 5) . modificoDesgasteRuedas (+ (velocidadMaxima auto * cambios / 10)) . modificoTiempo (+ 3 * cambios)) auto
+
+zigzagLoco :: Tramo
+zigzagLoco = zigzag 5
+
+casiCurva :: Tramo
+casiCurva = zigzag 1
+
+rulo :: Number -> Tramo
+rulo diametro auto = (modificoTiempo (+ 5 * diametro / velocidadMaxima auto) . modificoDesgasteRuedas (+ diametro * 1.5)) auto
+
+ruloClasico :: Tramo
+ruloClasico = rulo 13
+
+deseoDeMuerte :: Tramo
+deseoDeMuerte = rulo 26
+
+nivelJoya :: Auto -> Number
+nivelJoya auto
+    | tiempoCarrera auto < 50 = 1
+    | otherwise               = 2
+
+nivelDeJoyez :: [Auto] -> Number
+nivelDeJoyez = sumOf nivelJoya
+
+paraEntendidos :: [Auto] -> Bool
+paraEntendidos []           = True
+paraEntendidos (auto:autos) = estaEnBuenEstado auto && tiempoCarrera auto <= 200 && paraEntendidos autos
+
+redondearA2Decimales :: Number -> Number
+redondearA2Decimales numero = round (numero * 100) / 100
+
+{- ===================== ENTREGA 2 ===================== -}
+
+data Equipo = Equipo
+    {
+    nombreEquipo :: Number,
+    autos :: [Auto],
+    presupuesto :: Number
+    } deriving (Show)
+
+
+agregarAuto :: Equipo -> Auto -> Equipo
+agregarAuto equipo auto 
+                        | presupuesto equipo > costoInscripcion = equipo
+                                                                        {
+                                                                            autos = autos equipo ++ [auto],
+                                                                            presupuesto = (presupuesto equipo) - costoInscripcion
+                                                                        }
+                        | otherwise                             = equipo
+                        where costoInscripcion = ((* 1000).velocidadMaxima) auto
+
+
+cantidadReducida :: (b -> Number) -> b -> (b -> b) -> Number
+cantidadReducida desgaste auto funcion = (-) ((desgaste.funcion) auto) (desgaste auto) 
+
+repararAutos :: Equipo -> [Auto] -> Equipo
+repararAutos equipo [] = equipo
+repararAutos equipo (auto:restoAutos)
+    | presupuesto equipo >= costoReparacion = repararAutos equipoActualizado restoAutos
+    | otherwise                             = repararAutos equipo restoAutos -- No alcanza, pasa al siguiente auto
+  where
+    costoReparacion   = cantidadReducida desgasteChasis auto reparar * 500
+    equipoActualizado = equipo 
+        { 
+            presupuesto = presupuesto equipo - costoReparacion,
+            autos = reparar auto : autos equipo 
+        }
+
+repararEquipo :: Equipo -> Equipo
+repararEquipo equipo = repararAutos equipo (autos equipo)
+
+nitroAutos :: Equipo -> [Auto] -> Equipo
+nitroAutos equipo [] = equipo
+nitroAutos equipo (auto:restoAutos)
+    | presupuesto equipo >= costoReparacion = nitroAutos equipoActualizado restoAutos
+    | otherwise                             = nitroAutos equipo restoAutos -- No alcanza, pasa al siguiente auto
+  where
+    costoReparacion   = velocidadMaxima auto * 100
+    equipoActualizado = equipo 
+        { 
+            presupuesto = presupuesto equipo - costoReparacion,
+            autos = ponerNitro auto : autos equipo 
+        }
+
+nitroEquipo :: Equipo -> Equipo
+nitroEquipo equipo = nitroAutos equipo (autos equipo)
+
+ferrarizarAutos :: Equipo -> [Auto] -> Equipo
+ferrarizarAutos equipo [] = equipo
+ferrarizarAutos equipo (auto:restoAutos)
+    | (presupuesto equipo >= costoReparacion) && (not.(== "Ferrari").marca) auto = ferrarizarAutos equipoActualizado restoAutos
+    | otherwise                                                                  = ferrarizarAutos equipo restoAutos -- No alcanza, pasa al siguiente auto
+  where
+    costoReparacion   = 3500
+    equipoActualizado = equipo 
+        { 
+            presupuesto = presupuesto equipo - costoReparacion,
+            autos = desarmadero "Ferrari" "F90" auto : autos equipo 
+        }
+
+ferrarizarEquipo :: Equipo -> Equipo
+ferrarizarEquipo equipo = ferrarizarAutos equipo (autos equipo)
+
--- TP/tp-funcional-pisteando/src/Spec.hs
+++ TP/tp-funcional-pisteando/src/Spec.hs
@@ -5,41 +5,232 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Función estaEnBuenEstado" $ do
-    
-    it "Un Peugeot no debería estar en buen estado" $ do
+  describe "2.a - Estado de salud del auto" $ do
+    it "Un auto marca Peugeot no está en buen estado" $ do
       estaEnBuenEstado peugeot `shouldBe` False
+      
+    it "Un auto marca Lamborghini, con tiempo en pista de 99 segundos y desgaste de chasis 7, está en buen estado" $ do
+      estaEnBuenEstado (lamborghini { tiempoCarrera = 99 }) `shouldBe` True
+      
+    it "Un auto marca Fiat, con tiempo en pista de 99 segundos y desgaste de chasis 33, no está en buen estado" $ do
+      estaEnBuenEstado (fiat { tiempoCarrera = 99 }) `shouldBe` False
+      
+    it "Un auto marca Ferrari que tiene 130 segundos de tiempo en pista con desgaste de ruedas 50 y chasis 30 está en buen estado" $ do
+      estaEnBuenEstado (ferrari { tiempoCarrera = 130, desgasteRuedas = 50, desgasteChasis = 30 }) `shouldBe` True
+      
+    it "Un auto marca Ferrari que tiene 15 segundos de tiempo en pista con desgaste de ruedas 50 y chasis 45 no está en buen estado" $ do
+      estaEnBuenEstado (ferrari { tiempoCarrera = 15, desgasteRuedas = 50, desgasteChasis = 45 }) `shouldBe` False
+      
+    it "Un auto marca Ferrari que tiene 150 segundos de tiempo en pista con desgaste de ruedas 70 y chasis 30 no está en buen estado" $ do
+      estaEnBuenEstado (ferrari { tiempoCarrera = 150, desgasteRuedas = 70, desgasteChasis = 30 }) `shouldBe` False
 
-    it "Un Lamborghini con 99s de tiempo y 7 de desgaste de chasis debería estar en buen estado" $ do
-      estaEnBuenEstado lamborghini `shouldBe` True
+  describe "2.b - Saber si un auto no da más" $ do
+    it "Un auto de marca Ferrari con desgaste de ruedas 20 y chasis 90 no da más (primer apodo empieza con 'La ')" $ do
+      noDaMas (ferrari { desgasteRuedas = 20, desgasteChasis = 90 }) `shouldBe` True
+      
+    it "Un auto de marca Ferrari con desgaste de ruedas 90 y chasis 20 da para más" $ do
+      noDaMas (ferrari { desgasteRuedas = 90, desgasteChasis = 20 }) `shouldBe` False
+      
+    it "Un auto de marca Lamborghini con desgaste de ruedas 90 y chasis 20 no da más" $ do
+      noDaMas (lamborghini { desgasteRuedas = 90, desgasteChasis = 20 }) `shouldBe` True
+      
+    it "Un auto de marca Lamborghini base da para más" $ do
+      noDaMas lamborghini `shouldBe` False
 
-    it "Un Fiat con 99s de tiempo y 33 de desgaste de chasis no debería estar en buen estado" $ do
-      estaEnBuenEstado fiat `shouldBe` False
+  describe "2.c - Saber si un auto es un chiche" $ do
+    it "Un auto de marca Lamborghini es un chiche (apodos pares, chasis 7)" $ do
+      esChiche lamborghini `shouldBe` True
+      
+    it "Un auto de marca Lamborghini con desgaste de ruedas 90 y chasis 20 no es un chiche" $ do
+      esChiche (lamborghini { desgasteRuedas = 90, desgasteChasis = 20 }) `shouldBe` False
+      
+    it "Un auto de marca Ferrari con desgaste de ruedas 20 y chasis 90 no es un chiche (impar y chasis 90)" $ do
+      esChiche (ferrari { desgasteRuedas = 20, desgasteChasis = 90 }) `shouldBe` False
+      
+    it "Un auto de marca Ferrari es un chiche (apodos impares y cero desgaste de chasis)" $ do
+      esChiche ferrari `shouldBe` True
 
-    it "Un Ferrari con 130s de tiempo, 50 de ruedas y 30 de chasis debería estar en buen estado" $ do
-      estaEnBuenEstado ferrari1 `shouldBe` True
+  describe "2.d - Saber si un auto es una joya" $ do
+    it "Un auto de marca Peugeot es una joya (cero desgaste y un apodo)" $ do
+      esJoya peugeot `shouldBe` True
+      
+    it "Un auto de marca Ferrari no es una joya (no tiene desgaste pero tiene más de un apodo)" $ do
+      esJoya ferrari `shouldBe` False
 
-    it "Un Ferrari con 15s de tiempo, 50 de ruedas y 45 de chasis no debería estar en buen estado" $ do
-      estaEnBuenEstado ferrari2 `shouldBe` False
+  describe "2.e - Conocer el nivel de chetez" $ do
+    it "Un auto de marca Ferrari tiene un valor de 180 (20 * 3 apodos * 3 letras del modelo)" $ do
+      nivelDeChetez ferrari `shouldBe` 180
 
-    it "Un Ferrari con 150s de tiempo, 70 de ruedas y 30 de chasis no debería estar en buen estado" $ do
-      estaEnBuenEstado ferrari3 `shouldBe` False
+  describe "2.f - Capacidad supercalifragilisticaespialidosa" $ do
+    it "Un auto de marca Ferrari tiene un valor de 7 (cantidad de letras contando el espacio en 'La nave')" $ do
+      capacidadSupercalifragilisticaespialidosa ferrari `shouldBe` 7
 
--- Definiciones de los autos de la tabla
-peugeot :: Auto
-peugeot = Auto "Peugeot" "208" 0 0 150 [] 0
+  describe "2.g - Qué tan riesgoso es un auto" $ do
+    it "Un auto de marca Lamborghini tiene valor 29.2 (está en buen estado)" $ do
+      riesgoso lamborghini `shouldBe` 29.2
+      
+    it "Un auto de marca Fiat tiene valor 237.6 (no está en buen estado, multiplica por 2)" $ do
+      riesgoso fiat `shouldBe` 237.6
 
+  describe "3. Manos a la obra" $ do
+    describe "3.a - Reparar un auto" $ do
+      it "Reparar un auto de marca Fiat deja 0 en ruedas y 4.95 en chasis" $ do
+        let fiatReparado = reparar fiat
+        desgasteRuedas fiatReparado `shouldBe` 0 -- [cite: 236]
+        desgasteChasis fiatReparado `shouldBe` 4.95 -- [cite: 236]
+        
+      it "Reparar un auto de marca Ferrari mantiene sus desgastes en 0 y 0" $ do
+        let ferrariReparado = reparar ferrari
+        desgasteRuedas ferrariReparado `shouldBe` 0 -- [cite: 236]
+        desgasteChasis ferrariReparado `shouldBe` 0 -- [cite: 236]
+
+    describe "3.b - Aplicar penalidad" $ do
+      it "Aplicar penalidad de 20 segundos a un auto Ferrari con tiempo 10 segundos en pista lo deja en 30" $ do
+        tiempoCarrera (penalizar 20 (ferrari { tiempoCarrera = 10 })) `shouldBe` 30 -- [cite: 237]
+        
+      it "Aplicar penalidad de 0 segundos a un auto Ferrari con tiempo 10 lo mantiene igual" $ do
+        tiempoCarrera (penalizar 0 (ferrari { tiempoCarrera = 10 })) `shouldBe` 10 -- [cite: 237]
+
+    describe "3.c - Poner nitro" $ do
+      it "Poner nitro a un Fiat le deja una velocidad máxima de 52.8" $ do
+        velocidadMaxima (ponerNitro fiat) `shouldBe` 52.8 -- [cite: 242]
+        
+      it "Poner nitro a un Fiat con velocidad máxima 0 mantiene su velocidad" $ do
+        velocidadMaxima (ponerNitro (fiat { velocidadMaxima = 0 })) `shouldBe` 0 -- [cite: 242]
+
+    describe "3.d - Bautizar un auto" $ do
+      it "Bautizar 'El diablo' a un Lamborghini hace que contenga ese apodo" $ do
+        apodos (bautizar "El diablo" lamborghini) `shouldContain` ["El diablo"] -- [cite: 246]
+        
+      it "Bautizar 'El diablo' a un Lamborghini sin apodos hace que sólo tenga ese apodo" $ do
+        apodos (bautizar "El diablo" (lamborghini { apodos = [] })) `shouldBe` ["El diablo"] -- [cite: 246]
+
+    describe "3.e - Desarmadero" $ do
+      it "Llevar un Fiat al desarmadero para cambiar por Tesla X cambia la marca a Tesla" $ do
+        marca (desarmadero "Tesla" "X" fiat) `shouldBe` "Tesla" -- [cite: 247]
+        
+      it "Llevar un Fiat al desarmadero para cambiar por Tesla X cambia el modelo a X" $ do
+        modelo (desarmadero "Tesla" "X" fiat) `shouldBe` "X" -- [cite: 247]
+        
+      it "Llevar un Fiat al desarmadero para cambiar por Tesla X lo deja sólo con el apodo 'Nunca Taxi'" $ do
+        apodos (desarmadero "Tesla" "X" fiat) `shouldBe` ["Nunca Taxi"] -- [cite: 247, 249]
+
+  describe "4. ¡Pistas!" $ do
+    describe "4.a - Tramo Curva" $ do
+      it "Transitar curva peligrosa con Ferrari deja ruedas en 15" $ do
+        desgasteRuedas (curvaPeligrosa ferrari) `shouldBe` 15 -- [cite: 260]
+      it "Transitar curva peligrosa con Ferrari mantiene chasis en 0" $ do
+        desgasteChasis (curvaPeligrosa ferrari) `shouldBe` 0 -- [cite: 260]
+      it "Transitar curva peligrosa con Ferrari deja tiempo en 23.5" $ do
+        redondearA2Decimales (tiempoCarrera (curvaPeligrosa ferrari)) `shouldBe` 9.23 -- [cite: 260]
+        
+      it "Transitar curva tranca con Ferrari deja ruedas en 15" $ do
+        desgasteRuedas (curvaTranca ferrari) `shouldBe` 15 -- [cite: 260]
+      it "Transitar curva tranca con Ferrari mantiene chasis en 0" $ do
+        desgasteChasis (curvaTranca ferrari) `shouldBe` 0 -- [cite: 260]
+      it "Transitar curva tranca con Ferrari deja tiempo en 48.5" $ do
+        redondearA2Decimales (tiempoCarrera (curvaTranca ferrari)) `shouldBe` 16.92 -- [cite: 260]
+
+    describe "4.b - Tramo Recto" $ do
+      it "Transitar tramo recto classic con Ferrari deja chasis en 7.15 y tiempo en 11" $ do
+        let ferrariPost = tramoRectoClassic ferrari
+        desgasteChasis ferrariPost `shouldBe` 7.15 -- [cite: 267]
+        tiempoCarrera ferrariPost `shouldBe` 11 -- [cite: 267]
+        
+      it "Transitar tramito con Ferrari deja chasis en 2.6 y tiempo en 4" $ do
+        let ferrariPost =  tramito ferrari
+        desgasteChasis ferrariPost `shouldBe` 2.6 -- [cite: 267]
+        tiempoCarrera ferrariPost `shouldBe` 4 -- [cite: 267]
+
+    describe "4.c - Tramo ZigZag" $ do
+      it "Transitar zigZagLoco con Ferrari deja chasis en 5, ruedas en 32.5 y tiempo en 15" $ do
+        let ferrariPost =  zigzagLoco ferrari
+        desgasteChasis ferrariPost `shouldBe` 5 -- [cite: 276]
+        desgasteRuedas ferrariPost `shouldBe` 32.5 -- [cite: 276]
+        tiempoCarrera ferrariPost `shouldBe` 15 -- [cite: 276]
+        
+      it "Transitar casiCurva con Ferrari deja chasis en 2.6, ruedas en 5.0 y tiempo en 3" $ do
+        let ferrariPost =  casiCurva ferrari
+        desgasteChasis ferrariPost `shouldBe` 5.0 -- [cite: 276]
+        desgasteRuedas ferrariPost `shouldBe` 6.5 -- [cite: 276]
+        tiempoCarrera ferrariPost `shouldBe` 3 -- [cite: 276]
+
+    describe "4.d - Tramo Rulo" $ do
+      it "Transitar ruloClasico con Ferrari deja chasis en 0, ruedas en 19.5 y tiempo en 1" $ do
+        let ferrariPost =  ruloClasico ferrari
+        desgasteChasis ferrariPost `shouldBe` 0 -- [cite: 282]
+        desgasteRuedas ferrariPost `shouldBe` 19.5 -- [cite: 282]
+        tiempoCarrera ferrariPost `shouldBe` 1 -- [cite: 284]
+        
+      it "Transitar deseoDeMuerte con Ferrari deja chasis en 0, ruedas en 39.0 y tiempo en 2" $ do
+        let ferrariPost =  deseoDeMuerte ferrari
+        desgasteChasis ferrariPost `shouldBe` 0 -- [cite: 284]
+        desgasteRuedas ferrariPost `shouldBe` 39.0 -- [cite: 284]
+        tiempoCarrera ferrariPost `shouldBe` 2 -- [cite: 284]
+
+  describe "5. Opción de Recursión" $ do
+    describe "5.a - Nivel de Joyez" $ do
+      it "Ferrari con tiempo 49 y Peugeot con tiempo 50 dan un nivel de joyez total de 3" $ do
+        let autos = [ferrari { tiempoCarrera = 49 }, peugeot { tiempoCarrera = 50 }]
+        nivelDeJoyez autos `shouldBe` 3 -- [cite: 292]
+
+    describe "5.b - Grupo para Entendidos" $ do
+      it "Ferrari con tiempo 201 y Ferrari con tiempo 200 no es para entendidos (supera el tiempo)" $ do
+        let autos = [ferrari { tiempoCarrera = 201 }, ferrari { tiempoCarrera = 200 }]
+        paraEntendidos autos `shouldBe` False -- [cite: 298]
+        
+      it "Ferrari con tiempo 200 y un Peugeot no es para entendidos (Peugeot no está en buen estado)" $ do
+        let autos = [ferrari { tiempoCarrera = 200 }, peugeot]
+        paraEntendidos autos `shouldBe` False -- [cite: 298]
+        
+      it "Ferrari con tiempo 200 y Lamborghini con tiempo 200 es para entendidos" $ do
+        let autos = [ferrari { tiempoCarrera = 200 }, lamborghini { tiempoCarrera = 200 }]
+        paraEntendidos autos `shouldBe` True -- [cite: 298]
+
+-- a. Auto Ferrari [cite: 130, 131]
+ferrari :: Auto
+ferrari = Auto {
+    marca           = "Ferrari",
+    modelo          = "F50",
+    desgasteChasis  = 0,
+    desgasteRuedas  = 0,
+    velocidadMaxima = 65,
+    tiempoCarrera   = 0,
+    apodos          = ["La nave", "El fierro", "Ferrucho"]
+}
+
+-- b. Auto Lamborghini [cite: 132]
 lamborghini :: Auto
-lamborghini = Auto "Lamborghini" "Aventador" 0 7 350 [] 99
+lamborghini = Auto {
+    marca           = "Lamborghini",
+    modelo          = "Diablo",
+    desgasteChasis  = 7,
+    desgasteRuedas  = 4,
+    velocidadMaxima = 73,
+    tiempoCarrera   = 0,
+    apodos          = ["Lambo", "La bestia"]
+}
 
+-- c. Auto Fiat [cite: 133, 134]
 fiat :: Auto
-fiat = Auto "Fiat" "600" 0 33 110 [] 99
+fiat = Auto {
+    marca           = "Fiat",
+    modelo          = "600",
+    desgasteChasis  = 33,
+    desgasteRuedas  = 27,
+    velocidadMaxima = 44,
+    tiempoCarrera   = 0,
+    apodos          = ["La Bocha", "La bolita", "Fitito"]
+}
 
-ferrari1 :: Auto
-ferrari1 = Auto "Ferrari" "F40" 50 30 320 [] 130
-
-ferrari2 :: Auto
-ferrari2 = Auto "Ferrari" "F50" 50 45 325 [] 15
-
-ferrari3 :: Auto
-ferrari3 = Auto "Ferrari" "Enzo" 70 30 350 [] 150
+-- d. Auto Peugeot [cite: 137, 138]
+peugeot :: Auto
+peugeot = Auto {
+    marca           = "Peugeot",
+    modelo          = "504",
+    desgasteChasis  = 0,
+    desgasteRuedas  = 0,
+    velocidadMaxima = 40,
+    tiempoCarrera   = 0,
+    apodos          = ["El rey del desierto"]
+}
